7862c28164ce44ad5d25d877a55808767a5b1b46
cyrusjetson <jetsoncyrus@gmail.com>
2025-06-14 17:44:59 +0530

    code update

---
+++ src/Main.java
@@ -0,0 +1,134 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Main {
+    public static void main(String[] args) {
+
+        String str = " Hai   bro  ";
+        String[] stringArr = new String[]{"Hi", "Be", "bye", "see", "hello", "Hello", "DoRa", "  ", " ", "", new String("hello")};
+        int[] nums = new int[]{2, 4, 5, 3, 6, 7, 8, 9, 2, 9};
+
+        //// ------- Streams qns --------
+        ///  easy
+        // sum of all numbers in a list
+//        System.out.println(Arrays.stream(nums).sum());
+
+        // convert a list of strings to their lengths
+//        System.out.println(Arrays.stream(stringArr).map(String::length).toList());
+
+        // get unique strings
+//        System.out.println(Arrays.stream(stringArr).distinct().toList());
+//        System.out.println(Arrays.stream(stringArr).filter(x -> !x.isEmpty()).collect(Collectors.toSet()));
+
+        // get duplicate strings
+//        Set<String> seen = new HashSet<>();
+//        System.out.println(Arrays.stream(stringArr).filter(x -> !seen.add(x)).collect(Collectors.toSet()));
+
+        // remove duplicates from a list using distinct
+//        System.out.println(Arrays.stream(nums).distinct().boxed().toList());
+
+        // find minimum and maximum in list
+//        System.out.println("Minimum: " + Arrays.stream(nums).boxed().min(Integer::compare));
+//        System.out.println("Maximum: " + Arrays.stream(nums).boxed().max(Integer::compare));
+
+        // find min and max length non-empty string
+//        System.out.println(Arrays.stream(stringArr).filter(x -> !x.isEmpty()).min(Comparator.comparingInt(String::length)).orElse(null));
+//        System.out.println(Arrays.stream(stringArr).filter(x -> !x.isEmpty()).max(Comparator.comparingInt(String::length)).orElse(null));
+
+        // find min and max length non-empty strings
+//        int minLength = Arrays.stream(stringArr).filter(x -> !x.trim().isEmpty()).mapToInt(String::length).min().orElse(0);
+//        int maxLength = Arrays.stream(stringArr).filter(x -> !x.trim().isEmpty()).mapToInt(String::length).max().orElse(0);
+//        System.out.println("Strings with same min length: " + Arrays.stream(stringArr).filter(x -> !x.trim().isEmpty() && x.length() == minLength).toList());
+//        System.out.println("Strings with same max length: " + Arrays.stream(stringArr).filter(x -> !x.trim().isEmpty() && x.length() == maxLength).toList());
+
+        // filter non-empty strings from a list
+//        System.out.println("non-empty strings: " + Arrays.stream(stringArr).filter(x -> !x.isEmpty()).toList());
+
+        // finding list of all strings starts with "h" or "H"
+//        System.out.println(Arrays.stream(stringArr).filter(x -> x.startsWith("h") || x.startsWith("H")).toList());
+
+        // find first element starting with 'h'
+//        System.out.println(Arrays.stream(stringArr).filter(x -> x.startsWith("h") || x.startsWith("H")).findFirst().orElse(null));
+
+        // finding even numbers from a list
+//        System.out.println(Arrays.stream(nums).filter(x -> (x & 1) == 0).boxed().toList());
+
+        // count elements in list
+//        System.out.println(Arrays.stream(nums).count());
+
+        // Sorting list of strings alphabetically
+//        System.out.println(Arrays.stream(stringArr).filter(x -> !x.trim().isEmpty()).sorted().toList());
+
+        // Sorting strings based on string length
+//        System.out.println("length(asc) and alphabetic(asc)");
+//        System.out.println(Arrays.stream(stringArr).filter(x -> !x.trim().isEmpty())
+//                .sorted(Comparator.comparingInt(String::length)
+//                        .thenComparing(String::compareTo)).toList());
+//        System.out.println("length(asc) and alphabetic(desc)");
+//        System.out.println(Arrays.stream(stringArr).filter(x -> !x.trim().isEmpty())
+//                .sorted(Comparator.comparingInt(String::length)
+//                        .thenComparing(Comparator.reverseOrder())).toList());
+//        System.out.println("length(desc) and alphabetic(desc)");
+//        System.out.println(Arrays.stream(stringArr).filter(x -> !x.trim().isEmpty())
+//                .sorted(Comparator.comparingInt(String::length).reversed().thenComparing(Comparator.reverseOrder())).toList());
+//        System.out.println("length(desc) and alphabetic(asc)");
+//        System.out.println(Arrays.stream(stringArr).filter(x -> !x.trim().isEmpty())
+//                .sorted(Comparator.comparingInt(String::length).reversed().thenComparing(String::compareTo)).toList());
+
+        // finding distinct elements in stream
+//        System.out.println(Arrays.stream(nums).distinct());
+
+        // finding unique elements in stream with set
+//        Set<Integer> set = new HashSet<>();
+//        System.out.println(Arrays.stream(nums).boxed().filter(set::add).toList());
+
+        // finding duplicate elements
+//        Set<Integer> set = new HashSet<>();
+//        System.out.println(Arrays.stream(nums).filter(x -> !set.add(x)).boxed().collect(Collectors.toSet()));
+
+        // finding frequency map using streams
+        // -- Long count --
+//        Map<Integer, Long> freq = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(n -> n, Collectors.counting()));
+        //  -- to Integer
+//        Map<Integer, Integer> freq = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(n -> n,
+//                Collectors.collectingAndThen(Collectors.counting(), Long::intValue)));
+//        freq = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(n -> n,
+//                Collectors.counting()));
+//        System.out.println(freq.entrySet().stream().filter(x -> x.getValue() > 1).toList());
+//        Map<Integer, Integer> finalFreq = freq.entrySet().stream().filter(x -> x.getValue() > 1)
+//                .collect(Collectors.toMap(Map.Entry::getKey,
+//                        Map.Entry::getValue));
+//        System.out.println(finalFreq);
+
+// group a list of objects by a field eg., group employee objects by department
+
+        // find second largest number in the list
+//        System.out.println(Arrays.stream(nums).sorted().boxed().);
+
+        // check if all elements match a condition
+
+        // convert a list of strings to a map (string -> length)
+
+        // flatten a lists
+
+        // remove null or empty strings from a list
+
+        /// hard level
+        // find the longest string in a list
+
+        // top n elements from a list
+
+        // custom collector to join strings with delimiters, prefixes, and suffixes
+
+        // partition a list into two (even / odd) using Collectors.partitioningBy
+
+        // parallel stream performance test (compare stream and parallel stream)
+
+        // calculate frequency of each character in a string
+
+        // chain multiple stream operations efficiently
+
+        // implement own collector (Collector interface)
+    }
+}
